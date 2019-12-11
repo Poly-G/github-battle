261e729f5b6acec83578ce253ef2cbd7c5752fb4
Poly Goveia <poly.goveia@gmail.com>
2019-12-11 08:37:36 -0800

    create playerInput component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./App.css";
 import Popular from "./components/Popular";
+import Battle from "./components/Battle";
 
 function App() {
   return (
     <div className="App">
-      <Popular />
+      <Battle />
     </div>
   );
 }
